847d0088e0f1126beb0cb7120eec5afb91517523
zzaizzai <junsai0530@gmail.com>
2023-07-16 17:45:54 +0900

    print

--- ExcelControl.py
+++ ExcelControl.py
@@ -1,45 +1,54 @@
+from typing import Any
+
 import openpyxl
 
 
 class ExcelControl():
 
-    def __init__(self, excel_name = None):
+    def __init__(self, excel_name=None):
 
         self._workbook = openpyxl.Workbook()
-        
-        if excel_name != None :
+
+        if excel_name:
             self._workbook = openpyxl.load_workbook(filename=excel_name)
 
-
-        print(self.workbook.sheetnames)
-        print("Active: ", self.workbook.active.title)
         self._worksheet = self.workbook.active
-        print(self.worksheet)
-        print(self.worksheet.cell(row=1 , column=1).value)
-        self._cell = self.worksheet.cell(row=1 , column=1)
-        print(self.cell)
-
+        self._cell = self.worksheet.cell(row=1, column=1)
 
     @property
     def workbook(self) -> openpyxl.Workbook:
         return self._workbook
-    
+
     @workbook.setter
-    def workbook_setter(self, workbook):
+    def workbook(self, workbook: openpyxl.Workbook) -> None:
         self._workbook = workbook
 
     @property
     def worksheet(self):
         return self._worksheet
-    
+
     @worksheet.setter
-    def worksheet_setter(self, worksheet):
+    def worksheet(self, worksheet):
         self._worksheet = worksheet
-    
+
     @property
     def cell(self):
         return self._cell
 
+    def set_cell_value(self, value: Any) -> None:
+        self.cell.value = value
+
+    def print_cell_value(self) -> None:
+        print(self.cell.value)
+
+    def print_all(self) -> None:
+        for row in self.worksheet.iter_rows():
+            print([row[i].value for i in range(len(row))])
+
+
 if __name__ == "__main__":
-    ev = ExcelControl()
-    
+    ev = ExcelControl("data_sample.xlsx")
+    # ev.set_cell_value(1)
+    print(ev.workbook)
+    ev.print_cell_value()
+    ev.print_all()
